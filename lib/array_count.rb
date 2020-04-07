43aaa722153e8b09dfaea4da342c8f81c87d573e
Stephanie Cheney <steph@scheneyphotography.com>
2020-04-07 23:39:26 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,7 +1,11 @@
 def count_strings(array)
+  array.count do |element| element=='""'
+end 
   # Return the total number of strings in the provided array using the count enumerable
 end
 
 def count_empty_strings(array)
+   array.count do |string| string == ""
+ end 
   # Return the total number of EMPTY strings in the provided array using the count enumerable
 end
